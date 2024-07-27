2a682630731e333e7cc0f52e6d939154d2655219
Mag <u.magistr@gmail.com>
2024-07-27 16:54:19 +0200

    Add easy and hard

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,6 @@ use dotenv::dotenv;
 use std::env;
 
 // Fetch the daily LeetCode question
-// Returns the URL of the daily question if found
-// Returns None if no daily question is found
-// Returns an error if an HTTP request error occurs
-// The function is asynchronous because it makes an HTTP request
 async fn fetch_leetcode_daily_question(client: &Client) -> Result<Option<String>, Box<dyn std::error::Error + Send + Sync>> {
     let query = r#"
     {
@@ -34,7 +30,7 @@ async fn fetch_leetcode_daily_question(client: &Client) -> Result<Option<String>
         .json::<HashMap<String, Value>>()
         .await?;
 
-    println!("Responce from leetcode arrived.");
+    println!("Response from leetcode arrived.");
     if let Some(data) = response.get("data") {
         if let Some(active_daily_coding_challenge_question) = data.get("activeDailyCodingChallengeQuestion") {
             if let Some(link) = active_daily_coding_challenge_question.get("link") {
@@ -49,11 +45,56 @@ async fn fetch_leetcode_daily_question(client: &Client) -> Result<Option<String>
     Ok(None)
 }
 
-// Send the daily LeetCode challenge to the chat
-async fn send_daily_challenge(bot: Bot, chat_id: ChatId, client: Client) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    println!("Sending daily challenge...");
-    if let Some(url) = fetch_leetcode_daily_question(&client).await? {
-        let message = bot.send_message(chat_id, format!("Today's LeetCode Challenge: {}", url))
+// Fetch a LeetCode question based on difficulty
+async fn fetch_leetcode_question(client: &Client, difficulty: &str) -> Result<Option<String>, Box<dyn std::error::Error + Send + Sync>> {
+    let query = format!(r#"
+    {{
+        "query": "query randomQuestion($categorySlug: String, $filters: QuestionListFilterInput) {{ randomQuestion(categorySlug: $categorySlug, filters: $filters) {{ titleSlug }} }}",
+        "variables": {{
+            "categorySlug": "",
+            "filters": {{
+                "difficulty": "{}"
+            }}
+        }},
+        "operationName": "randomQuestion"
+    }}
+    "#, difficulty);
+    println!("Sending request to leetcode for {} question...", difficulty);
+    let response = client
+        .post("https://leetcode.com/graphql/")
+        .header("Content-type", "application/json")
+        .header("Origin", "leetcode.com")
+        .header("User-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36")
+        .body(query)
+        .send()
+        .await?
+        .json::<HashMap<String, Value>>()
+        .await?;
+
+    println!("Response from leetcode for {} question arrived.", difficulty);
+    if let Some(data) = response.get("data") {
+        if let Some(random_question) = data.get("randomQuestion") {
+            if let Some(title_slug) = random_question.get("titleSlug") {
+                if let Some(title_slug_str) = title_slug.as_str() {
+                    println!("{} question found.", difficulty);
+                    return Ok(Some(format!("https://leetcode.com/problems/{}", title_slug_str)));
+                }
+            }
+        }
+    }
+
+    Ok(None)
+}
+
+// Send the LeetCode challenge to the chat
+async fn send_leetcode_challenge(bot: Bot, chat_id: ChatId, client: Client, difficulty: &str) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let url = match difficulty {
+        "daily" => fetch_leetcode_daily_question(&client).await?,
+        _ => fetch_leetcode_question(&client, difficulty).await?,
+    };
+    println!("Sending message to chat...");
+    if let Some(url) = url {
+        let message = bot.send_message(chat_id, format!("Today's LeetCode Challenge ({}) : {}", difficulty, url))
             .parse_mode(ParseMode::Html)
             .disable_web_page_preview(true)
             .send()
@@ -61,12 +102,10 @@ async fn send_daily_challenge(bot: Bot, chat_id: ChatId, client: Client) -> Resu
         bot.pin_chat_message(chat_id, message.id)
         .disable_notification(true)
         .send().await?;
-    println!("Daily challenge sent.");
     } else {
-        bot.send_message(chat_id, "No daily question found.")
+        bot.send_message(chat_id, format!("No {} question found.", difficulty))
             .send()
             .await?;
-        println!("No daily question found.");
     }
     Ok(())
 }
@@ -112,8 +151,19 @@ async fn main() {
     // Calculate the duration until the next trigger time
     let duration = duration_until_next_trigger(trigger_time);
     let start = Instant::now() + duration;
+    println!("Sending messages immediately...");
+    // Send the messages immediately
+    if let Err(err) = send_leetcode_challenge(bot.clone(), chat_id, client.clone(), "daily").await {
+        eprintln!("Error sending daily challenge: {:?}", err);
+    }
+    if let Err(err) = send_leetcode_challenge(bot.clone(), chat_id, client.clone(), "Easy").await {
+        eprintln!("Error sending easy challenge: {:?}", err);
+    }
+    if let Err(err) = send_leetcode_challenge(bot.clone(), chat_id, client.clone(), "Hard").await {
+        eprintln!("Error sending hard challenge: {:?}", err);
+    }
 
-    // Spawn a task to send the daily challenge
+    // Spawn a task to send the daily challenge at the trigger time
     let bot_clone = bot.clone();
     let client_clone = client.clone();
     println!("Spawning task to send daily challenge...");
@@ -123,8 +173,40 @@ async fn main() {
             println!("Waiting for next trigger...");
             interval.tick().await;
             println!("Triggered.");
-            if let Err(err) = send_daily_challenge(bot_clone.clone(), chat_id, client_clone.clone()).await {
+            if let Err(err) = send_leetcode_challenge(bot_clone.clone(), chat_id, client_clone.clone(), "daily").await {
                 eprintln!("Error sending daily challenge: {:?}", err);
+            }
+        }
+    });
+
+    // Spawn a task to send an easy problem at the trigger time
+    let bot_clone_easy = bot.clone();
+    let client_clone_easy = client.clone();
+    println!("Spawning task to send easy challenge...");
+    tokio::spawn(async move {
+        let mut interval = interval_at(start, Duration::from_secs(60 * 60 * 24));
+        loop {
+            println!("Waiting for next trigger...");
+            interval.tick().await;
+            println!("Triggered.");
+            if let Err(err) = send_leetcode_challenge(bot_clone_easy.clone(), chat_id, client_clone_easy.clone(), "Easy").await {
+                eprintln!("Error sending easy challenge: {:?}", err);
+            }
+        }
+    });
+
+    // Spawn a task to send a hard problem at the trigger time
+    let bot_clone_hard = bot.clone();
+    let client_clone_hard = client.clone();
+    println!("Spawning task to send hard challenge...");
+    tokio::spawn(async move {
+        let mut interval = interval_at(start, Duration::from_secs(60 * 60 * 24));
+        loop {
+            println!("Waiting for next trigger...");
+            interval.tick().await;
+            println!("Triggered.");
+            if let Err(err) = send_leetcode_challenge(bot_clone_hard.clone(), chat_id, client_clone_hard.clone(), "Hard").await {
+                eprintln!("Error sending hard challenge: {:?}", err);
             }
         }
     });
